f36d8439d89acb20b4c04a41099921bd0f9abdf5
Cao Ngoc Minh <minhcn2205@gmail.com>
2026-09-19 01:49:30 +0700

    Update start script and format privacy page

--- start.bat
+++ start.bat
@@ -2,5 +2,5 @@
 title Portfolio Server
 echo Dang khoi dong server portfolio...
 start "" "http://localhost:3000"
-"C:\Program Files\Adobe\Adobe Photoshop 2025\node.exe" server.js
+node server.js
 pause
